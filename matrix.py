8ec6b002faf5a1fbd023a67e9d8526859150fbd2
eccentricayman <aahmed3@stuy.edu>
2017-02-17 11:54:33 -0500

    print matrix done

--- matrix.py
+++ matrix.py
@@ -2,8 +2,20 @@ import math
 
 
 def print_matrix( matrix ):
-    pass
-
+    retStr = "";
+    for i in range(len(matrix)):
+        #print the left border
+        retStr += "|"
+        #print the row, except for the last number
+        for j in range(len(matrix[i]) - 1):
+            retStr += str(matrix[i][j]) + "\t"
+        #print last number without tab
+        retStr += str(matrix[i][j])
+        #print right border with newline
+        retStr += "|\n"
+    #get rid of last newline
+    print retStr[:-1]
+    
 def ident( matrix ):
     pass
 
@@ -14,9 +26,6 @@ def scalar_mult( matrix, s ):
 def matrix_mult( m1, m2 ):
     pass
 
-
-
-
 def new_matrix(rows = 4, cols = 4):
     m = []
     for c in range( cols ):
@@ -24,3 +33,5 @@ def new_matrix(rows = 4, cols = 4):
         for r in range( rows ):
             m[c].append( 0 )
     return m
+
+print_matrix(new_matrix())
